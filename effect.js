fd964be10662fc93ccd8ec7cb4d5b773c2aeded0
Farhan Nur Fuadi <farhannfs5121@gmail.com>
2021-01-03 19:41:10 +0700

    update

--- effect.js
+++ effect.js
@@ -170,15 +170,15 @@ $('document').ready(function(){
 		$('#b7').attr('id','b77')
 		$('#b8').attr('id','b88')
 		$('#b9').attr('id','b99')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
-		$('#b88').animate({top:240, left: vw+350},500);
-		$('#b99').animate({top:240, left: vw+450},500);
+		$('#b11').animate({top:240, left: vw-450},500);
+		$('#b22').animate({top:240, left: vw-350},500);
+		$('#b33').animate({top:240, left: vw-250},500);
+		$('#b44').animate({top:240, left: vw-150},500);
+		$('#b55').animate({top:240, left: vw-50},500);
+		$('#b66').animate({top:240, left: vw+50},500);
+		$('#b77').animate({top:240, left: vw+150},500);
+		$('#b88').animate({top:240, left: vw+250},500);
+		$('#b99').animate({top:240, left: vw+350},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
